1bf73e23f1f4734bcf4adcf1be29fd94416a4449
Sama20Os <sama16mohamed@gmail.com>
2023-03-20 14:34:37 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - print some words
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}
