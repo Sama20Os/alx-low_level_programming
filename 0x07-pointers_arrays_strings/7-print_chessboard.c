54baff82f01472d35cdcdab2e2236c0565134d68
Sama20Os <sama16mohamed@gmail.com>
2023-04-03 15:33:01 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_chessboard - prints chessboard.
+ *
+ * @a: rows.
+ * Return: nothing.
+ */
+void void print_chessboard(char (*a)[8])
+{
+	int bsd, bod;
+
+	for (bsd = 0; bsd < 8; bsd++)
+	{
+		for (bod = 0; bod < 8; bod++)
+		{
+			_putchar(a[bsd][bod]);
+		}
+		_putchar('\n');
+	}
+}
