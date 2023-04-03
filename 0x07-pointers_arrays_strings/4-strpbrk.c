577b9414d491a4f6a234d22b9a4c1b0d69a401b6
Sama20Os <sama16mohamed@gmail.com>
2023-04-03 15:00:10 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+include "main.h"
+
+
+/**
+ * _strpbrk - search for a string
+ * @s: string
+ * @accept: string also but to suit
+ * Return: byte in s that suits a byte in accept
+ * or NULL if nothing is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int bsd, bod;
+	char *p;
+
+	bsd = 0;
+	while (s[bsd] != '\0')
+	{
+		bod = 0;
+		while (accept[bod] != '\0')
+		{
+			if (accept[bod] == s[bsd])
+			{
+				p = &s[bsd];
+				return (p);
+			}
+			bod++;
+		}
+		bsd++;
+	}
+
+	return (0);
+}
