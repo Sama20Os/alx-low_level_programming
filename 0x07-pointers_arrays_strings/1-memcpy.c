fe13418bebc0fcbbc62f68964ecc62140e7a6369
Sama20Os <sama16mohamed@gmail.com>
2023-04-03 14:29:33 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * *_memcpy - copy spaces inside memory
+ * @dest: memory spaces
+ * @src: source
+ * @n: length of src to copy
+ *
+ * Return: the pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int bsd;
+
+	for (bsd = 0; bsd < n; bsd++)
+	{
+		dest[bsd] = src[bsd];
+	}
+	return (dest);
+}
