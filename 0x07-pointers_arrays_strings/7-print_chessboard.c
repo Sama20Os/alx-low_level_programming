6cd816094b24579f56bdc59f842f9c3fe4ea3816
Sama20Os <sama16mohamed@gmail.com>
2023-04-04 19:15:28 -0700

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints chessboard.
- *
- * @a: rows.
- * Return: nothing.
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
-void void print_chessboard(char (*a)[8])
-{
-	int bsd, bod;
 
-	for (bsd = 0; bsd < 8; bsd++)
+void print_chessboard(char (*a)[8])
+
+{
+
+	int i;
+
+	int j;
+
+
+	for (i = 0; i < 8; i++)
+
 	{
-		for (bod = 0; bod < 8; bod++)
-		{
-			_putchar(a[bsd][bod]);
-		}
+
+		for (j = 0; j < 8; j++)
+
+			_putchar(a[i][j]);
+
 		_putchar('\n');
+
 	}
+
 }
